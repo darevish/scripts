6171895e45155aa75caafa30561cd28a343de694
darevish <dar3vish@gmail.com>
2015-01-17 01:50:24 +0100

    brightness setting script for zenbook ux303ln on ubuntu

---
+++ brightness.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import sys
+
+minBrightness = 100;
+
+maxBrightnessFile = open("/sys/class/backlight/intel_backlight/max_brightness", "r");
+maxBrightness = int(maxBrightnessFile.read());
+maxBrightnessFile.close();
+
+brightnessFile = open("/sys/class/backlight/intel_backlight/brightness", "r+")
+brightness = int(brightnessFile.read())
+brightnessFile.seek(0)
+brightnessFile.truncate()
+
+try:
+    step = int( sys.argv[1] );
+except (IndexError, ValueError):
+    step = 0;
+
+if step > 0:
+    if brightness <= ( maxBrightness - step ):
+        brightness += step
+    else:
+        brightness = maxBrightness
+elif step < 0:
+    if brightness >= (minBrightness - step):
+        brightness += step
+    else:
+        brightness = minBrightness
+
+brightnessFile.write( str(brightness) + "\n" );
+brightnessFile.close();
